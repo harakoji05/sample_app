a2c46c2d8ff5f15595edb1ba2f53b2e3acd6d006
harakoji05 <harakoji.0514@gmail.com>
2020-05-06 21:30:26 +0900

    [finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
 
  	get 'todolists/:id' => 'todolists#show', as:'todolist'
 
+ 	get 'todolist/:id/edit' => 'todolists#edit', as:'edit_todolist'
+
+ 	patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+
 
 end
