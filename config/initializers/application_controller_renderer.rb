1a1477bd8050236b7ee5e85920263036ca9e32d6
harakoji05 <harakoji.0514@gmail.com>
2020-05-07 22:04:44 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4780485eed161cf0dabd93427053963240379e8e31d407bdda0609a2ebdf2cbbb2c38b1d8c781caf004042d0f9b89b5f8784017efdaa65303227159be505e7e9'
